4963d6d9273d670f8c6254b37004a9ef6407e726
Ashlenator <ababjac@gmail.com>
2021-06-01 15:54:08 -0400

    Working through tutorial

--- scripts/code-handout.R
+++ scripts/code-handout.R
@@ -1,93 +1,60 @@
 # temporarily load RSQLite so that the version shows in sessionInfo()
 library(RSQLite)
-
-
-
+library(tidyverse)
 detach(package:RSQLite, unload=TRUE)
+
+######################### INTRO TO R ##############################
+
+
 ### Creating objects in R
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ### Challenge
 ##
 ## What are the values after each statement in the following?
 ##
 ## mass <- 47.5            # mass?
+(mass <- 47.5)
 ## age  <- 122             # age?
+(age <- 122)
 ## mass <- mass * 2.0      # mass?
+(mass <- mass * 2.0)
 ## age  <- age - 20        # age?
+(age <-  age - 20)
 ## mass_index <- mass/age  # mass_index?
-
-
-
-
-
-
-
-
-
-
-
-
+(mass_index <- mass/age)
 
 
 
 ### Vectors and data types
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ## ## We’ve seen that atomic vectors can be of type character, numeric, integer, and
 ## ## logical. But what happens if we try to mix these types in a single
 ## ## vector?
-## 
+print("R will implicitly type cast objects while trying to avoid losing information.")
+
 ## ## What will happen in each of these examples? (hint: use `class()` to
 ## ## check the data type of your object)
-## num_char <- c(1, 2, 3, "a")
-## 
-## num_logical <- c(1, 2, 3, TRUE)
-## 
-## char_logical <- c("a", "b", "c", TRUE)
-## 
-## tricky <- c(1, 2, 3, "4")
+num_char <- c(1, 2, 3, "a")
+print(class(num_char))
+
+num_logical <- c(1, 2, 3, TRUE)
+print(class(num_logical))
+
+char_logical <- c("a", "b", "c", TRUE)
+print(class(char_logical))
+
+tricky <- c(1, 2, 3, "4")
+print(class(tricky))
+
 ## 
 ## ## Why do you think it happens?
+print("vectors can only contain a single data type")
 ## 
 ## 
 ## ## How many values in `combined_logical` are `"TRUE"` (as a character) in the
 ## ## following example:
-## combined_logical <- c(num_logical, char_logical)
+combined_logical <- c(num_logical, char_logical)
+print(length(subset(combined_logical, combined_logical == "TRUE")))
 ## 
 ## ## You've probably noticed that objects of different types get
 ## ## converted into a single, shared type within a vector. In R, we call
@@ -96,94 +63,60 @@ detach(package:RSQLite, unload=TRUE)
 ## ## whereby some types get preferentially coerced into other types. Can
 ## ## you draw a diagram that represents the hierarchy of how these data
 ## ## types are coerced?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("logical becomes character/numeric, numeric becomes character")
 
 ### Challenge (optional)
 ##
 ## * Can you figure out why `"four" > "five"` returns `TRUE`?
+print("Strings are compared alphabetically")
 
 
-
-
-
+## ### Challenge
 heights <- c(63, 69, 60, 65, NA, 68, 61, 70, 61, 59, 64, 69, 63, 63, NA, 72, 65, 64, 70, 63, 65)
 
-# 1.
+# 1. Using this vector of heights in inches, create a new vector with the NAs removed.
 heights_no_na <- heights[!is.na(heights)] 
 # or
 heights_no_na <- na.omit(heights)
 # or
 heights_no_na <- heights[complete.cases(heights)]
 
-# 2.
+# 2. Use the function `median()` to calculate the median of the `heights` vector.
 median(heights, na.rm = TRUE)
 
-# 3.
+# 3. Use R to figure out how many people in the set are taller than 67 inches.
 heights_above_67 <- heights_no_na[heights_no_na > 67]
 length(heights_above_67)
 
-## ### Challenge
-## 1. Using this vector of heights in inches, create a new vector with the NAs removed.
-##
-##    heights <- c(63, 69, 60, 65, NA, 68, 61, 70, 61, 59, 64, 69, 63, 63, NA, 72, 65, 64, 70, 63, 65)
-##
-## 2. Use the function `median()` to calculate the median of the `heights` vector.
-##
-## 3. Use R to figure out how many people in the set are taller than 67 inches.
+
+######################### STARTING WITH DATA #############################
+
+
 ### Loading the survey data
 
-## download.file(url = "https://ndownloader.figshare.com/files/2292169",
-##               destfile = "data_raw/portal_data_joined.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
+download.file(url = "https://ndownloader.figshare.com/files/2292169",
+               destfile = "data/raw/portal_data_joined.csv")
+surveys <- read_csv("data/raw/portal_data_joined.csv")
 
 ## Challenge
 ## Based on the output of `str(surveys)`, can you answer the following questions?
-##
-## * What is the class of the object `surveys`?
-## * How many rows and how many columns are in this object?
+str(surveys)
 
+## * What is the class of the object `surveys`?
+print(class(surveys))
+## * How many rows and how many columns are in this object?
+print(nrow(surveys))
+print(ncol(surveys))
 
 
 ## Indexing and subsetting data frames
-
-
-
-
-
-
-
-
 
 ### Challenges:
 ###
 ### 1. Create a `data.frame` (`surveys_200`) containing only the
 ###    data in row 200 of the `surveys` dataset.
-###
+surveys_200 <- surveys[200, ]
+
 ### 2. Notice how `nrow()` gave you the number of rows in a `data.frame`?
 ###
 ###      * Use that number to pull out just that last row in the data frame
@@ -191,88 +124,98 @@ length(heights_above_67)
 ###        sure it's meeting expectations.
 ###      * Pull out that last row using `nrow()` instead of the row number
 ###      * Create a new data frame object (`surveys_last`) from that last row
-###
+surveys_last <- surveys[nrow(surveys), ]
+tail(surveys, 1)
+
 ### 3. Use `nrow()` to extract the row that is in the middle of the
 ###    data frame. Store the content of this row in an object named
 ###    `surveys_middle`.
-###
+surveys_middle <- surveys[nrow(surveys)/2, ]
+
 ### 4. Combine `nrow()` with the `-` notation above to reproduce the behavior of
 ###    `head(surveys)`, keeping just the first through 6th rows of the surveys
 ###    dataset.
+surveys_head <- surveys[-(7:nrow(surveys)), ]
+
 
 
 ### Factors
-
-
-
-
-
 sex <- factor(c("male", "female", "female", "male"))
-
-
-
-
-
-
 
 ### Challenges:
 ###
 ### 1. Change the columns `taxa` and `genus` in the `surveys` data frame into a 
 ###    factor.
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
 ###
 ### 2. Using the functions you learned before, can you find out...
 ###
 ###      * How many rabbits were observed?
+summary(surveys$taxa)
 ###      * How many different genera are in the `genus` column?
+nlevels(surveys$genus)
 
 
-
-
+#Converting Data
 year_fct <- factor(c(1990, 1983, 1977, 1998, 1990))
 as.numeric(year_fct)               # Wrong! And there is no warning...
 as.numeric(as.character(year_fct)) # Works...
 as.numeric(levels(year_fct))[year_fct]    # The recommended way.
 
 ## bar plot of the number of females and males captured during the experiment:
+surveys$sex <- factor(surveys$sex)
 plot(surveys$sex)
 
-
-
-
-
-
+surveys$sex <- addNA(surveys$sex)
+levels(surveys$sex)[3] <- "NA"
+plot(surveys$sex)
 
 ## Challenges
 ##
 ## * Rename "F" and "M" to "female" and "male" respectively.
+levels(surveys$sex) <- c('male', 'female', 'undetermined')
 ## * Now that we have renamed the factor level to "undetermined", can you recreate the
 ##   barplot such that "undetermined" is first (before "female")
+surveys$sex <- factor(surveys$sex, levels = c("undetermined", "female", "male"))
+plot(surveys$sex)
 
 
 
 ## ## Challenge:
 ## ##  There are a few mistakes in this hand-crafted `data.frame`,
 ## ##  can you spot and fix them? Don't hesitate to experiment!
-## animal_data <- data.frame(
-##       animal = c(dog, cat, sea cucumber, sea urchin),
-##       feel = c("furry", "squishy", "spiny"),
-##       weight = c(45, 8 1.1, 0.8)
-##       )
+animal_data <- data.frame(
+      animal = c("dog", "cat", "sea cucumber", "sea urchin"),
+      feel = c("furry", "furry", "squishy", "spiny"),
+      weight = c(45, 8, 1.1, 0.8)
+      )
 
 
 
 ## ## Challenge:
+
+country_climate <- data.frame(country = c("Canada", "Panama", "South Africa", "Australia"),
+                               climate = c("cold", "hot", "temperate", "hot/temperate"),
+                               temperature = c(10, 30, 18, "15"),
+                               northern_hemisphere = c(TRUE, TRUE, FALSE, "FALSE"),
+                               has_kangaroo = c(FALSE, FALSE, FALSE, 1))
 ## ##   Can you predict the class for each of the columns in the following
 ## ##   example?
+str(country_climate)
 ## ##   Check your guesses using `str(country_climate)`:
 ## ##   * Are they what you expected? Why? why not?
+print("They are implicitly cast wrong to strings due to data type errors")
 ## ##   * What would you need to change to ensure that each column had the
 ## ##     accurate data type?
-## country_climate <- data.frame(country = c("Canada", "Panama", "South Africa", "Australia"),
-##                                climate = c("cold", "hot", "temperate", "hot/temperate"),
-##                                temperature = c(10, 30, 18, "15"),
-##                                northern_hemisphere = c(TRUE, TRUE, FALSE, "FALSE"),
-##                                has_kangaroo = c(FALSE, FALSE, FALSE, 1))
+country_climate <- data.frame(country = c("Canada", "Panama", "South Africa", "Australia"),
+                              climate = c("cold", "hot", "temperate", "hot/temperate"),
+                              temperature = c(10, 30, 18, 15),
+                              northern_hemisphere = c(TRUE, TRUE, FALSE, FALSE),
+                              has_kangaroo = c(FALSE, FALSE, FALSE, TRUE))
+
+
 ## ## Pipes Challenge:
 ## ##  Using pipes, subset the data to include animals collected
 ## ##  before 1995, and retain the columns `year`, `sex`, and `weight.`
